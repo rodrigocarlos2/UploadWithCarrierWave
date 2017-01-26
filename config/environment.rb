fbb9f3f50845391c2bebecbd375824501dfa6601
Rodrigo Carlos <rodrigo19962010@live.com>
2017-01-26 11:48:34 -0300

    Application maked with Rodrigo

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+
+require 'carrierwave/orm/activerecord'
+
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
---
+++ public/uploads/tmp/1485441938-17933-0003-6340/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+int main(int arg, char *args[]){
+
+int edson = 1;
+
+
+
+if(strcmp(args[1],"-ivan")){
+printf("\nDesligando");
+}
+else{
+printf("for -h to more options");
+}
+
+
+
+
+
+return edson;
+}
